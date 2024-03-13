243ad1a684b07ae61aa9b31a8e4e143f823576d0
Max <126002400+MaxTrgb@users.noreply.github.com>
2024-03-13 18:51:11 +0200

    Add files via upload

---
+++ 0403wfCLass/0403wfCLass/CalculatorModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0403wfCLass
+{
+    public static class CalculatorModel
+    {
+        public static double numOne { get; set; }
+        public static double numTwo { get; set; }
+
+        public static double add(double numOne, double numTwo) { 
+            return numOne + numTwo;
+        }
+
+        public static double substract(double numOne, double numTwo)
+        {
+            return numOne - numTwo;
+        }
+
+        public static double multiply(double numOne, double numTwo)
+        {
+            return numOne * numTwo;
+        }
+
+        public static double divide(double numOne, double numTwo)
+        {
+            return numOne / numTwo;
+        }
+    }
+}
---
+++ 0403wfCLass/0403wfCLass/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace _0403wfCLass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double numOne = Int32.Parse(textBox1.Text);
+            double numTwo = Int32.Parse(textBox2.Text);
+            double result = CalculatorModel.add(numOne, numTwo);
+            label3.Text = result.ToString();  
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double numOne = Int32.Parse(textBox1.Text);
+            double numTwo = Int32.Parse(textBox2.Text);
+            double result = CalculatorModel.substract(numOne, numTwo);
+            label3.Text = result.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double numOne = Int32.Parse(textBox1.Text);
+            double numTwo = Int32.Parse(textBox2.Text);
+            double result = CalculatorModel.multiply(numOne, numTwo);
+            label3.Text = result.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double numOne = Int32.Parse(textBox1.Text);
+            double numTwo = Int32.Parse(textBox2.Text);
+            double result = CalculatorModel.divide(numOne, numTwo);
+            label3.Text = result.ToString();
+        }
+    }
+}
