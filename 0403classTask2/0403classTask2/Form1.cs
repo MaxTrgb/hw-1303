164224a8b4f71cc8242d9bed31205ef9b2a970e5
Max <126002400+MaxTrgb@users.noreply.github.com>
2024-03-13 19:29:52 +0200

    Add files via upload

---
+++ 0403classTask2/0403classTask2/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0403classTask2
+{
+    public partial class Form1 : Form
+    {
+        private NoteModel noteModel;
+        public Form1()
+        {
+            InitializeComponent();
+            noteModel = new NoteModel();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string noteName = textBox1.Text;
+            string noteText = textBox2.Text;
+            string date = textBox3.Text;
+          
+            noteModel.AddNoteToListView(listView1, noteName, noteText, date);
+
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                int selectedIndex = listView1.SelectedItems[0].Index;
+
+                noteModel.DeleteNoteFromListView(listView1, selectedIndex);
+            }
+            else
+            {
+                MessageBox.Show("Please select a note to delete.");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //noteModel.SaveNotesToJson(listView1, "notes.json");
+            //MessageBox.Show("Notes saved to notes.json");
+        }
+    }
+}
---
+++ 0403classTask2/0403classTask2/NoteModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.IO;
+//using System.Text.Json;
+using System.Windows.Forms;
+
+namespace _0403classTask2
+{
+    //public class Note
+    //{
+    //    public string Name { get; set; }
+    //    public string Text { get; set; }
+    //    public string Date { get; set; }
+    //}
+
+    internal class NoteModel
+    {
+        public void AddNoteToListView(ListView listView, string noteName, string noteText, string date)
+        {
+
+            ListViewItem item = new ListViewItem(noteName);
+            item.SubItems.Add(noteText);
+            item.SubItems.Add(date);
+
+
+            listView.Items.Add(item);
+        }
+        public void DeleteNoteFromListView(ListView listView, int index)
+        {
+            if (index >= 0 && index < listView.Items.Count)
+            {
+                listView.Items.RemoveAt(index);
+            }
+        }
+
+        //public void SaveNotesToJson(ListView listView, string filePath)
+        //{
+        //    List<Note> notes = new List<Note>();
+
+            
+        //    foreach (ListViewItem item in listView.Items)
+        //    {
+        //        string noteName = item.SubItems[0].Text;
+        //        string noteText = item.SubItems[1].Text;
+        //        string date = item.SubItems[2].Text;
+
+        //        Note note = new Note
+        //        {
+        //            Name = noteName,
+        //            Text = noteText,
+        //            Date = date
+        //        };
+
+        //        notes.Add(note);
+        //    }
+
+        //    string json = JsonSerializer.Serialize(notes);
+        //    File.WriteAllText(filePath, json);
+        //}
+    }
+}
+
+
